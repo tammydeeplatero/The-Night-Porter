092fc4785d87ba498842d76116a2af885ff099f3
user911667 <user911667@tx008.local>
2019-05-30 21:52:37 -0500

    Added button to change background and change text label fonts to dark grey

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+        
+        //Change the text color of every label
+        //First, get all of the views
+        let everything = view.subviews
+        for view in everything
+        {
+            if(view is UILabel)
+            {
+                let currentView = view as! UILabel
+                currentView.textColor = UIColor.darkGray
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
